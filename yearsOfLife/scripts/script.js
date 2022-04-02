24d422fcec0e69ca7a3c09c8ad932decf4f7877c
jeffer <jeffersunde72@gmail.com>
2022-04-02 20:28:52 +0200

    Finishing

--- yearsOfLife/scripts/script.js
+++ yearsOfLife/scripts/script.js
@@ -1,4 +1,4 @@
-const yearOfBith = document.querySelector("input")
+const birth = document.querySelector("input")
 const button = document.querySelector("button")
 const container = document.querySelector("main")
 
@@ -8,8 +8,33 @@ container.appendChild(output)
 const date = new Date()
 
 button.addEventListener("click", () => {
-    var yearsOfLife = date.getFullYear() - Number(yearOfBith.value)
-    var daysOfLife = yearsOfLife * 365
+    if (birth.value.length == 0) {
+        output.innerText = `Por favor, digite o seu ano de nascimento.`
+    } else {
+        var actualMonth = Number(date.getMonth()) + 1
+        var yearOfBirth = birth.value.slice(0, 4)
+        var monthOfBirth = birth.value.slice(5)
+        var yearsOfLife = date.getFullYear() - Number(yearOfBirth)
+        var differenceBetween = actualMonth - monthOfBirth
 
-    output.innerText = `Voce tem aproximadamente ${daysOfLife} dias de vida.`
+
+        if (yearsOfLife == 0) {
+            console.log(differenceBetween)
+            var daysOfLife = differenceBetween * 30
+            console.log(daysOfLife)
+        } else {
+            if (Number(actualMonth) <= monthOfBirth) {
+                
+                if (differenceBetween !== 0) {
+                    yearsOfLife--
+                    var daysOfLife = yearsOfLife * 365
+                    daysOfLife += (365 - (differenceBetween * 30))
+                } else {
+                    var daysOfLife = yearsOfLife * 365
+                }
+            }    
+        }
+        
+        output.innerText = `Voce tem aproximadamente ${daysOfLife} dias de vida.`
+    }
 })
